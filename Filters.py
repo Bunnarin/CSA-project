d50ecede00bdbc15ca7efd4ba3469b0f13f3968f
Bunnarin <chrunbunnarin857@gmail.com>
2025-03-12 11:32:27 +0700

    new jobs

--- Filters.py
+++ Filters.py
@@ -1,8 +1,10 @@
 import os
-# solo 1
+# solo 1: me
+path = "D:\CS\CSA"
 def get_path(path): 
     ...
     return files
+
 def filter_types(files, types):
     ...
     return files
@@ -12,10 +14,20 @@ def filter_size(files, size, mode):
 def filter_date(files, date, mode):
     ...
     return files
+
+# solo 2: vutha
+files = [
+    "D:\CS\CSA\book1.py",
+    "D:\CS\CSA\book2.py",
+    "D:\CS\CSA\book3.py",
+    "D:\CS\CSA\b.py",
+]
+wildcard = 'book*.py'
 def filter_wildcard(files, wildcard):
     ...
     return files
-# solo 2
-def find_replace(files, find, replace):
-    ...
-    return files
+files = [
+    "D:\CS\CSA\book1.py",
+    "D:\CS\CSA\book2.py",
+    "D:\CS\CSA\book3.py",
+]
