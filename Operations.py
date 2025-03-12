b49be54a18fbf650b7dff9f46627757a372b4539
Bunnarin <chrunbunnarin857@gmail.com>
2025-03-12 11:04:41 +0700

    csa

---
+++ Operations.py
@@ -0,0 +1,11 @@
+import os
+# solo 1
+def move(files, destination):
+    pass
+def copy(files, destination):
+    pass
+def delete(files):
+    pass
+# solo 2
+def find_replace(files, find, replace):
+    pass
