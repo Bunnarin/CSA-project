f6f5804a7b88ef6900873a711e8ac4392ed6738c
Bunnarin <chrunbunnarin857@gmail.com>
2025-03-21 11:40:09 +0700

    modularised the code with classes

--- src/Interface.py
+++ src/Interface.py
@@ -1,36 +1,34 @@
-# must handle invalid input
-# DANA
-def get_input():
-    # get a directory
-    print('''
-    filter by:
-        1.type
-        2.size
-        3.date
-        4.wildcard
-    ''')
-    return filters
-# example filters:
-filters = {
-    'path': 'C:/Users/',
-    'types': ['txt', 'pdf'],
-    'size': (100, 'kb', 'lt/eq/gt'),
-    'date': ('2020-01-01-hh-mm', 'c/m/a', 'before/on/after'),
-    'wildcard': 'hello*'
-}
+class Interface:
+    def __init__(self): pass
 
-# thanou
-def get_output():
-    print('''
-    operations:
-        1.move
-        2.copy
-        3.delete
-        4.find and replace
-    ''')
-    return actions
-# example actions:
+    def get_input(self):
+        # get a directory
+        print('''
+        filter by:
+            1.type
+            2.size
+            3.date
+            4.wildcard
+        ''')
+        filters = {}
+        return filters
+
+    # thanou
+    def get_output(self, files):
+        if not files: 
+            print("no files were selected with the input filters. exiting.")
+            return
+        print('''
+        operations:
+            1.move
+            2.copy
+            3.delete
+            4.find and replace
+        ''')
+        actions = {}
+        return actions
+# example result:
 actions = {
-    'type': 'copy',
-    'parameter': 'path, None, (find, replace)'
+    'type': 'copy/move/delete/find_replace',
+    'params': 'path/None/(find, replace)'
 }
