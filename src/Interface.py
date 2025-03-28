aeb8c563b7fb0c9c7d973f99b53b41ba7adf6d93
Bunnarin <chrunbunnarin857@gmail.com>
2025-03-28 11:00:34 +0700

    added dana's part

--- src/Interface.py
+++ src/Interface.py
@@ -13,11 +13,7 @@ class Interface:
         filters = {}
         return filters
 
-    # thanou
-    def get_output(self, files):
-        if not files: 
-            print("no files were selected with the input filters. exiting.")
-            return
+    def get_output(self):
         print('''
         operations:
             1.move
@@ -25,10 +21,24 @@ class Interface:
             3.delete
             4.find and replace
         ''')
-        actions = {}
+        choice = int(input("Enter any operations you want to operate between 1 and 4:  "))
+
+        if choice == 1:
+            move_to = input("Where do you want to move to? ")
+            actions = {'Type' : 'move','parameter' : move_to}
+            print(f"Moved to {move_to}")
+        elif choice == 2:
+            copy = input("Copy to where?  ")
+            actions = {'Type': 'copy', 'parameter':copy}
+            print(f"Done copied to {copy}")
+        elif choice == 3:
+            actions = {'Type': 'Delete', 'parameter': None}
+        elif choice == 4:
+            find = input("Enter the file you want to find: ")
+            replace = input("Replace with what?: ")
+            actions = {'Type':'find_replace', 'parameter': (find, replace)}
+        else:
+            print("Enter only any of these 4 operations: ")
+            choice = int(input("Enter: "))
+
         return actions
-# example result:
-# actions = {
-#     'type': 'copy/move/delete/find_replace',
-#     'params': 'path/None/(find, replace)'
-# }
